2b130e8c3491fbebdccf3d69d3e9b846cc8ac79b
Zakariae EL ASRI <72689460+elasriz@users.noreply.github.com>
2024-03-09 10:33:22 +0100

    Update display_all_phihp.py

--- scripts/display_all_phihp.py
+++ scripts/display_all_phihp.py
@@ -35,7 +35,7 @@ def display_agent(model_class,seed, alpha, episodes, env,  receding_horizon, hor
          
 if __name__ == "__main__":
 
-            seed = 1     
+            seed = 0     
 
 
         
